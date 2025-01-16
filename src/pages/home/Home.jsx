f80069800f21185ea206ed51af1932c608af6831
SabbirMeazee <mdsabbirmeazee69@gmail.com>
2025-01-16 18:14:05 +0600

    product and search added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,12 +3,14 @@ import Layout from '../../components/layout/Layout'
 import myContext from '../../context/data/myContext';
 import Herosection from '../../components/heroSection/Herosection';
 import Filter from '../../components/filter/Filter';
+import ProductCard from '../../components/ProductCard/ProductCard';
 
 function Home() {
     return (
         <Layout>
             <Herosection></Herosection>
             <Filter></Filter>
+            <ProductCard></ProductCard>
         </Layout>
     )
 }
