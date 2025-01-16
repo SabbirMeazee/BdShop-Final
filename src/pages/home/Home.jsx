69a199d52a0481e317e75ed61af476878db820ab
SabbirMeazee <mdsabbirmeazee69@gmail.com>
2025-01-16 18:04:43 +0600

    herò section added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React, { useContext } from 'react'
 import Layout from '../../components/layout/Layout'
 import myContext from '../../context/data/myContext';
+import Herosection from '../../components/heroSection/Herosection';
+import Filter from '../../components/filter/Filter';
 
 function Home() {
     return (
         <Layout>
-            <h1>Name : </h1>
-
+            <Herosection></Herosection>
+            <Filter></Filter>
         </Layout>
     )
 }
