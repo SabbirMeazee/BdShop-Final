d27db761e55ebc818181953e50f9ab5bdaad64bf
SabbirMeazee <mdsabbirmeazee69@gmail.com>
2025-01-16 16:58:17 +0600

    navbar responsive added

--- src/pages/Order/order.jsx
+++ src/pages/Order/order.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Layout from '../../components/layout/Layout'
+import myContext from '../../context/data/myContext';
 
 function order() {
+    const context = useContext(myContext);
+    const { name } = context;
+
     return (
         <Layout>
-            order
+            <h1>Name : {name}</h1>
         </Layout>
     )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,12 +3,10 @@ import Layout from '../../components/layout/Layout'
 import myContext from '../../context/data/myContext';
 
 function Home() {
-    const context = useContext(myContext);
-    console.log(context)
-    const { name } = context;
     return (
         <Layout>
-            <h1>Name : {name}</h1>
+            <h1>Name : </h1>
+
         </Layout>
     )
 }
