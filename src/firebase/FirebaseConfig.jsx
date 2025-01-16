c7333150418de1b996b0adff334c1989d2e9f2b1
SabbirMeazee <mdsabbirmeazee69@gmail.com>
2025-01-16 22:40:04 +0600

    firebase setup added

---
+++ src/firebase/FirebaseConfig.jsx
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth/cordova";
+import { getFirestore } from "firebase/firestore/lite";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDvUJoYY9CwMnZbXMQlvFGw2lzFoxuKQ7I",
+    authDomain: "bd-shop-final.firebaseapp.com",
+    projectId: "bd-shop-final",
+    storageBucket: "bd-shop-final.firebasestorage.app",
+    messagingSenderId: "857403525584",
+    appId: "1:857403525584:web:2d3e542bd5d852129191d5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const fireDB = getFirestore(app);
+const auth = getAuth(app)
+export { fireDB, auth } 
